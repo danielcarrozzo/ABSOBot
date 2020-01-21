cdb4e3b24595b3845e98f411867fd54548450170
Daniel Carrozzo <daniel.carrozzo@gmail.com>
2020-01-21 13:45:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!')
+  }
+})
+
+client.on('message', message => {
+  if (message.content.startsWith('!kick')) {
+    const member = message.mentions.members.first()
+
+    if (!member) {
+      return message.reply(
+        `Who are you trying to kick? You must mention a user.`
+      )
+    }
+
+    if (!member.kickable) {
+      return message.reply(`I can't kick this user. Sorry!`)
+    }
+
+    return member
+      .kick()
+      .then(() => message.reply(`${member.user.tag} was kicked.`))
+      .catch(error => message.reply(`Sorry, an error occured.`))
+  }
+})
+
+client.login(process.env.DISCORD_TOKEN)
