2a461c9eee8f36963516a48287421d91bff84dde
DARKRAYTA <32038954+DARKRAYTA@users.noreply.github.com>
2020-06-09 21:52:55 +0200

    nt4jgen

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.on('message', msg => {
   //if(msg.content.startsWith()){
   //}
   if (msg.content === 'Ciao') {
-    msg.channel.send('Ciao7')
+    msg.channel.send('Ciao')
   }
   if(msg.member.hasPermission(['KICK_MEMBERS', 'BAN_MEMBERS'])){
     if (msg.content.startsWith('SpamTag')) {
