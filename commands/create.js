3ab7209e78cabbb6c099ebc1bebe40fb294a7de6
DARKRAYTA <32038954+DARKRAYTA@users.noreply.github.com>
2020-07-10 11:27:03 +0200

    Patch per il crash con ra, l e vl e damn

--- commands/create.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'create',//anche c
-	description: 'Create all',
-	execute(message, args) {
-        var msg=message;
-        //creare il tutto
-          msg.channel.send('I created the base for all what you need to do!');
-	},
-};
--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'ping',
-  cooldown: 5,
-	description: 'Ping and Pong',
-	execute(message, args) {
-        var msg=message;
-      return msg.channel.send('Pong');
-    },
-};
